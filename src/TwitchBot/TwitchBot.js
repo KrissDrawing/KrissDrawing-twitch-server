2cd94aece1105f1df9a8a2e88f9fb8aaf77be185
krissdrawing <krx95@o2.pl>
2021-03-08 16:08:41 +0100

    Added light controll feature from twitch chat

--- src/TwitchBot/TwitchBot.js
+++ src/TwitchBot/TwitchBot.js
@@ -3,6 +3,7 @@ import { throttle } from "throttle-debounce";
 import { PubSub } from "apollo-server";
 import { chatClient } from "../TwitchApi/client.js";
 import { ReadSpentPoints } from "../../functions/localFunctions.js";
+import { hexToRgb, setYeelightLight } from "../utilities/lightControlls.js";
 
 await chatClient.connect();
 
@@ -32,6 +33,22 @@ const fetchRank = async () => {
   });
 };
 
+const lightMessage = throttle(15000, true, async (channel) => {
+  chatClient.say(
+    channel,
+    `Aby zmienić kolor piszemy "!led " + kod koloru HEX, np. z tej strony https://htmlcolorcodes.com/color-picker/ przykładowo "!led #ff0000"`
+  );
+});
+
+const lightControll = throttle(15000, true, async (message, channel) => {
+  let ledColor = hexToRgb(message.split(" ")?.[1]);
+  if (ledColor !== null) {
+    setYeelightLight(ledColor.r, ledColor.g, ledColor.b, 1);
+  } else {
+    lightMessage(channel);
+  }
+});
+
 //Chat commands
 const rankCommand = throttle(30000, true, async (channel) => {
   await fetchRank();
@@ -54,10 +71,52 @@ const discordCommand = throttle(30000, true, (channel) => {
     "Zapraszam na serwerek, można pogadać, poumawiać się na gierki, itp. https://discord.gg/uzr4e7sPxT"
   );
 });
-
 const socialsCommand = throttle(30000, true, (channel) => {
   instagramCommand(channel);
   youtubeCommand(channel);
+});
+
+export const commandsListener = chatClient.onMessage(async (channel, user, message, msg) => {
+  if (["!rank", "!rang", "!ranga", "!ranks", "!rangs"].some((command) => command === message)) {
+    rankCommand(channel);
+  }
+
+  if (
+    ["!socials", "!sociale", "!insta", "!fb", "!facebook", "!instagram", "!twitter"].some(
+      (command) => command === message
+    )
+  ) {
+    socialsCommand(channel);
+  }
+
+  if (["!discord", "!dc", "!ds", "!dsc", "!diskord"].some((command) => command === message)) {
+    discordCommand(channel);
+  }
+
+  if ("!points" === message || "!points" === message.split(" ")[0]) {
+    let checkUser = user;
+
+    if (message?.split(" ")?.[1]?.trim()) {
+      checkUser = message.split(" ")[1].trim();
+    }
+    let rewardObject = await ReadSpentPoints(checkUser);
+    if (!rewardObject) {
+      chatClient.say(channel, "KEKW Typ nawet nie odebrał nic LUL");
+    } else {
+      chatClient.say(
+        channel,
+        `${checkUser} odebrał nagrody ${rewardObject.rewards_count} razy, wydając łącznie ${rewardObject.points_spent} punktów KEKW`
+      );
+    }
+  }
+
+  if ("!led" === message.toLowerCase()) {
+    lightMessage(channel);
+  }
+
+  if ("!led" === message.toLowerCase().split(" ")[0] && message.toLowerCase().split(" ")[1]) {
+    lightControll(message, channel);
+  }
 });
 
 //discord alert timer;
@@ -76,35 +135,3 @@ setInterval(() => {
   instagramCommand("krissdrawing");
   pubsubChat.publish("instagramAlert", Date.now());
 }, 1000000);
-
-export const commandsListener = chatClient.onMessage(async (channel, user, message, msg) => {
-  if (["!rank", "!rang", "!ranga", "!ranks", "!rangs"].some((command) => command === message)) {
-    rankCommand(channel);
-  }
-  if (
-    ["!socials", "!sociale", "!insta", "!fb", "!facebook", "!instagram", "!twitter"].some(
-      (command) => command === message
-    )
-  ) {
-    socialsCommand(channel);
-  }
-  if (["!discord", "!dc", "!ds", "!dsc", "!diskord"].some((command) => command === message)) {
-    discordCommand(channel);
-  }
-  if ("!points" === message || "!points" === message.split(" ")[0]) {
-    let checkUser = user;
-
-    if (message?.split(" ")?.[1]?.trim()) {
-      checkUser = message.split(" ")[1].trim();
-    }
-    let rewardObject = await ReadSpentPoints(checkUser);
-    if (!rewardObject) {
-      chatClient.say(channel, "KEKW Typ nawet nie odebrał nic LUL");
-    } else {
-      chatClient.say(
-        channel,
-        `${checkUser} odebrał nagrody ${rewardObject.rewards_count} razy, wydając łącznie ${rewardObject.points_spent} punktów KEKW`
-      );
-    }
-  }
-});
--- src/utilities/lightControlls.js
+++ src/utilities/lightControlls.js
@@ -142,3 +142,19 @@ export const daylightLoop = (topic) => {
 
   return { r: 100, g: 100, b: 100, a: 0.1 };
 };
+
+export const hexToRgb = (hex) => {
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+};

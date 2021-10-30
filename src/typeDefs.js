7345425452b396ae72940d4470375717bf80bda2
krzysztof ziecina <kziecina@airnauts.com>
2021-10-30 17:24:20 +0200

    fix typo importing TwitchResolver in typedefs.js

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -3,7 +3,7 @@ import { mergeTypeDefs } from "@graphql-tools/merge";
 import { typeDefs as lightTypes } from "./types/Lights.js";
 import { typeDefs as loginTypes } from "./types/Login.js";
 import { typeDefs as alarmTypes } from "./types/Alarm.js";
-import { typeDefs as twitchTypes } from "./twitchApi/TwitchResolver.js";
+import { typeDefs as twitchTypes } from "./TwitchApi/TwitchResolver.js";
 
 const types = [lightTypes, loginTypes, alarmTypes, twitchTypes];
 
